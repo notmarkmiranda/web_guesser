2bdd664fb11fb92ce9c3e6322d3efe1ce4fc150e
Mark Miranda <notmarkmiranda@gmail.com>
2016-03-11 10:56:23 -0700

    iteration 3 done

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 
-x = rand(101)
+SECRET_NUMBER = rand(100)
+
+ def check_guess(guess)
+
+   if guess.nil?
+     message = ""
+   elsif guess.to_i > SECRET_NUMBER + 5
+     message = "Way too high!"
+   elsif guess.to_i > SECRET_NUMBER
+     message = "Too high!"
+   elsif guess.to_i < SECRET_NUMBER - 5
+     message = "Way too low!"
+   elsif guess.to_i < SECRET_NUMBER
+     message = "Too low!"
+   else
+     message = "THE SECRET NUMBER IS #{SECRET_NUMBER}! YOU GOT IT RIGHT!"
+   end
+
+ end
+
+
 
 get '/' do
-  erb:index, :locals => {:number => x}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb:index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
