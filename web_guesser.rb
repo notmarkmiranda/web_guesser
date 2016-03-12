9384c15d28752204aa25908fe6a881f57ed0c486
Mark Miranda <notmarkmiranda@gmail.com>
2016-03-12 10:28:44 -0800

    can't get color to work

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,8 +8,10 @@ SECRET_NUMBER = rand(100)
  def check_guess(guess)
 
    if guess.nil?
+     color = "red"
      message = ""
    elsif guess.to_i > SECRET_NUMBER + 5
+     color = "blue"
      message = "Way too high!"
    elsif guess.to_i > SECRET_NUMBER
      message = "Too high!"
@@ -28,5 +30,5 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb:index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb:index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
 end
