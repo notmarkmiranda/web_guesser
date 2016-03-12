24c9da93fa2b6858a54a5e44c9e4641014d3d991
Mark Miranda <notmarkmiranda@gmail.com>
2016-03-12 10:40:43 -0800

    i4: coloring

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,19 +8,29 @@ SECRET_NUMBER = rand(100)
  def check_guess(guess)
 
    if guess.nil?
-     color = "red"
      message = ""
+     color = "red"
+     [message, color]
    elsif guess.to_i > SECRET_NUMBER + 5
-     color = "blue"
      message = "Way too high!"
+     color = "blue"
+     [message, color]
    elsif guess.to_i > SECRET_NUMBER
      message = "Too high!"
+     color = "pink"
+     [message, color]
    elsif guess.to_i < SECRET_NUMBER - 5
      message = "Way too low!"
+     color = "green"
+     [message, color]
    elsif guess.to_i < SECRET_NUMBER
      message = "Too low!"
+     color = "orange"
+     [message, color]
    else
      message = "THE SECRET NUMBER IS #{SECRET_NUMBER}! YOU GOT IT RIGHT!"
+     color = "yellow"
+     [message, color]
    end
 
  end
@@ -29,6 +39,6 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"]
-  message = check_guess(guess)
-  erb:index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
+  response = check_guess(guess)
+  erb:index, :locals => {:number => SECRET_NUMBER, :message => response[0], :color => response[1]}
 end
